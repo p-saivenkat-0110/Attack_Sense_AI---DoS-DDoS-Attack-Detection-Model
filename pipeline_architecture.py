a1d7cfafbcc82d6cb2e58b6e6a32996ce4fabe2f
Peruri Sai Venkat <113287699+p-saivenkat-0110@users.noreply.github.com>
2025-04-22 22:14:16 +0530

    Added Pipeline Architecture code

---
+++ pipeline_architecture.py
@@ -0,0 +1,79 @@
+import threading
+import numpy as np
+import pandas as pd
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+from plyer import notification
+
+
+class Model:
+    def __init__(self, model_path):
+        self.model = tf.keras.models.load_model(model_path)
+
+    def predict(self, window_data):
+        window_data = window_data.reshape((1, window_data.shape[0], window_data.shape[1]))
+        prediction = self.model.predict(window_data, verbose=0)
+        return np.argmax(prediction)
+
+class WindowFetcher:
+    def __init__(self):
+        data_path = os.path.join(os.getcwd(),"Sample Dataset/stream.csv")
+        self.__df = pd.read_csv(data_path, parse_dates=['Timestamp'])
+        self.tmp = self.__df
+
+    def fetch_past_K_minute_data(self, timestamp, window_size):
+        df = self.__df.copy()
+        endTimestamp = timestamp.strftime('%Y-%m-%d %H:%M')
+        df['group'] = df['Timestamp'].dt.strftime('%Y-%m-%d %H:%M')
+        df = df.set_index('group')
+        minutes = sorted(list(set(df.index)))
+        endIndex = minutes.index(endTimestamp)+1
+        startIndex = max(0, endIndex-window_size)
+        windowed_df = df.loc[minutes[startIndex:endIndex],:].drop(['Timestamp','Label'],axis=1)
+        windowed_df = np.array(windowed_df.values)
+        return windowed_df
+    
+
+class ParallelExecuter(threading.Thread):
+    def __init__(self, name, model, window_fetcher, queue_in, queue_out, window_size):
+        super().__init__()
+        self.name = name
+        self.model = model
+        self.window_fetcher = window_fetcher
+        self.queue_in = queue_in
+        self.queue_out = queue_out
+        self.window_size = window_size
+        self.daemon = True
+        print(f"{self.name} initialized...")
+
+    def __notify(self, timestamp, attackType):
+        print(f"\033[1;31m[{self.name}] : ALERT!! {attackType} detected at {timestamp}\033[1;0m")
+        # notification.notify(
+        #             title=f"⚠️ {attackType} Attack Detected!!",
+        #             message=f"{self.name} model detected attack at {timestamp}",
+        #             timeout=5
+        #         )
+
+    def run(self):
+        while True:
+            if self.queue_in.empty():
+                continue
+            timestamp = self.queue_in.get()
+            window_input = self.window_fetcher.fetch_past_K_minute_data(timestamp, self.window_size)
+            if window_input.shape[0] == 0:
+                continue
+
+            pred = self.model.predict(window_input)
+            if pred == 1:
+                self.__notify(timestamp, "DOS")
+                continue
+            elif pred == 2:
+                self.__notify(timestamp, "DDOS")
+                continue
+            else:
+                if self.queue_out:
+                    print(f"[{self.name}] : Normal → Passing to next queue")
+                    self.queue_out.put(timestamp)
+                else:
+                    print(f"[{self.name}] : Normal -- Passed all checks !!")
